afa002aad62158fa844f3629de32ecbac1d6e462
Pauelep <depeapau@gmail.com>
2023-11-23 20:05:03 +0100

    dssd

--- maintwo.js
+++ maintwo.js
@@ -3,27 +3,3 @@ $(document).ready(function () {
 });
 
 const elementos = ["body", ".nav_overlay", ".barra"];
-
-function cambiarColorDeFondo() {
-  // Colores específicos para el degradado
-  const color1 = "#4F90FD";  // Azul
-  const color2 = "#00D14F";  // Rojo
-  const color3 = "#FD990F";  // Rojo
-
-  // Aplicar el degradado al fondo de cada elemento
-  for (let i = 0; i < elementos.length; i++) {
-    const elemento = document.querySelector(elementos[i]);
-    elemento.style.background = `linear-gradient(to right, ${color1}, ${color2},${color3})`;
-  }
-
-  // Restaurar el fondo original después de un segundo
-  setTimeout(() => {
-    for (let i = 0; i < elementos.length; i++) {
-      const elemento = document.querySelector(elementos[i]);
-      elemento.style.background = "";
-    }
-  }, 2000);
-}
-
-// Cambiar el color de fondo cada 5 segundos
-setInterval(cambiarColorDeFondo, 10000);
